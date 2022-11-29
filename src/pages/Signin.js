3fc6aae91c651a419979865d520c08e3a1a3b098
arta444 <artaaghili2002@gmail.com>
2022-11-28 20:37:49 -0800

    added more signin

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,4 +1,15 @@
 //import all from components 
-export default function Signin(){
-    return <h1>Signin</h1>
+import React from "react"
+function Signin(){
+    return(
+        <form>
+        <label for="fname">First name:</label><br></br>
+        <input type="text" id="fname" name="fname"></input><br></br>
+        <label for="lname">Last name:</label><br></br>
+        <input type="text" id="lname" name="lname"></input>
+        </form>
+
+    ); 
 }
+
+export default Signin;
