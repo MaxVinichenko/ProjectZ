6c9a48bf11006425d35c255da6d2078d2948cbb6
arta444 <artaaghili2002@gmail.com>
2022-11-28 20:41:48 -0800

    I added the sign in thing

--- src/ProjectZ.web.react/pages/Signin.js
+++ src/ProjectZ.web.react/pages/Signin.js
@@ -2,13 +2,14 @@
 import React from "react"
 function Signin(){
     return(
+        <div>
         <form>
         <label for="fname">First name:</label><br></br>
         <input type="text" id="fname" name="fname"></input><br></br>
         <label for="lname">Last name:</label><br></br>
         <input type="text" id="lname" name="lname"></input>
         </form>
-
+        </div>
     ); 
 }
 
