11f3047130c4bce986eb10ed17c6d72af2f07ae0
MaxVin <maxvin2002vinmax@gmail.com>
2022-11-28 20:59:50 -0800

    Home Page First Version

--- src/ProjectZ.web.react/pages/Home.js
+++ src/ProjectZ.web.react/pages/Home.js
@@ -1,3 +1,23 @@
+import React from "react";
+
 export default function Home(){
-    return <h1>Home</h1>
+    
+    return (
+    <div className="heroSection"> 
+        
+        <div className="heroTextDiv">
+            <h1>Send rental applications with one click.</h1>
+            <p className="heroText">Tired of filling out many different rental app forms? Fill one form out on our webste, and never fill another form again! </p>
+        </div>
+        
+        
+        
+        <image src="/images/sky.jpg"></image>
+
+    </div>
+    
+    
+    
+    )
+
 }
