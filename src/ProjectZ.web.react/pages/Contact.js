dcf91ce2e9af071e1143ecb49891a5f2235c99a5
arta444 <artaaghili2002@gmail.com>
2022-11-30 09:45:21 -0800

    trying to do contact

--- src/ProjectZ.web.react/pages/Contact.js
+++ src/ProjectZ.web.react/pages/Contact.js
@@ -2,24 +2,14 @@ import React from "react"
 
 
 export default function Contact(){
-    return <div class="container">
-    <h1 >Contact Us</h1>
-    <form target="_blank" action="https://formsubmit.co/your@email.com" method="POST">
-      <div class="form-group">
-        <div class="form-row">
-          <div class="col">
-            <input type="text" name="name" class="form-control" placeholder="Full Name" required/>
-          </div>
-          <div class="col">
-            <input type="email" name="email" class="form-control" placeholder="Email Address" required/>
-          </div>
-        </div>
-      </div>
-      <div class="form-group">
-        <textarea placeholder="Your Message" class="form-control" name="message" rows="10" required></textarea>
-      </div>
-      <button type="submit" class="btn btn-lg btn-dark btn-block">Submit Form</button>
-    </form>
+    return(
+<div className="container">
+  <div className="svghouse">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z" clipRule="evenodd" /></svg>
   </div>
+  <h1>Contact Us</h1>
+  
+</div>
+    ); 
 }
 
